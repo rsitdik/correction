642b7897b72d7f87295243c2aba4ef92b1f3d529
rsitdik <12r04m79s>
2019-10-22 18:51:42 +0300

    added catch instead throws

--- src/CorrectionTextFile.java
+++ src/CorrectionTextFile.java
@@ -3,14 +3,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CorrectionTextFile {
-    private static List<String> readFile(File source) throws IOException {
+    private static List<String> readFile(File source) {
         List<String> data = new ArrayList<>();
 
         try (BufferedReader reader = new BufferedReader(new FileReader(source))) {
             String s;
             while ((s = reader.readLine()) != null) {
                 s = s.replace("�", "\"")
-                        .replace("\n", " ")
                         .replaceAll("don\"t", "don\'t")
                         .replaceAll("specifc", "specific")
                         .replaceAll("Ta B le", "TABLE")
@@ -18,19 +17,23 @@ public class CorrectionTextFile {
                         .replaceAll("f le", "file");
                 data.add(s);
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         return data;
     }
 
-    private static void writeFile(List<String> data, File destination) throws IOException {
+    private static void writeFile(List<String> data, File destination) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(destination))) {
             for (String s : data) {
                 writer.write(s);
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         File source = new File("e:\\\\in.txt");
         File destination = new File("e:\\\\out.txt");
         List<String> data = readFile(source);
